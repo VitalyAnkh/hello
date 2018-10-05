22bb950db00b49205b65f7c8ca6bf97a595997cd
VitalyR <vitalyankh@gmail.com>
2018-10-05 15:23:01 +0800

    Add 404 page.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7879").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
@@ -13,14 +14,37 @@ fn main() {
 //    stream.write(response.as_bytes()).unwrap();
 //    stream.flush().unwrap();
 }
-
+//fn handle_connection(mut stream: TcpStream) {
+//    let mut buffer = [0; 512];
+//    stream.read(&mut buffer).unwrap();
+//
+//    let contents = fs::read_to_string("hello.html").unwrap();
+//
+//    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+//
+//    stream.write(response.as_bytes()).unwrap();
+//    stream.flush().unwrap();
+//}
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 256];
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    println!("{}", String::from_utf8_lossy(&buffer[..]));
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+//    println!("{}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line="HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents=fs::read_to_string("404.html").unwrap();
+        let response=format!("{}{}",status_line,contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
